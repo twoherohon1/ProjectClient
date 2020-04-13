9b82a4a0773761e5221626dc1604fa4653b02a6c
twoherohon1 <twoherohon1@hotmail.com>
2020-04-13 12:21:04 +0700

    logincss

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,15 +1,9 @@
-// React core
 import React, { Component } from 'react';
-
-// Firebase.
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 
-// Styles
-import './SignInScreen.css'; // This uses CSS modules.
 
-// Initialize Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyDq-5ifSogscSWdRcqorSLbmKEBZKZ3_IE",
     authDomain: "authen-6c28d.firebaseapp.com",
@@ -21,42 +15,32 @@ const firebaseConfig = {
     measurementId: "G-H20XVGTP3X"
 };
 
-// Instantiate a Firebase app.
 firebase.initializeApp(firebaseConfig);
 
-class SignInScreen extends Component {
+class LoginForm extends Component {
 
-  // The component's Local state.
   state = {
-    isSignedIn: false // Local signed-in state.
+    isSignedIn: false 
   };
 
-  // Configure FirebaseUI.
   uiConfig = {
-    // Popup signin flow rather than redirect flow.
     signInFlow: 'popup',
-    // We will display Google , Facebook , Etc as auth providers.
     signInOptions: [
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
       firebase.auth.GithubAuthProvider.PROVIDER_ID,
       firebase.auth.EmailAuthProvider.PROVIDER_ID,
     ],
     callbacks: {
-      // Avoid redirects after sign-in.
       signInSuccess: () => false
     }
   };
 
-  // Listen to the Firebase Auth state and set the local state.
   componentDidMount() {
     this.unregisterAuthObserver = firebase.auth().onAuthStateChanged(
         (user) => this.setState({isSignedIn: !!user})
     );
   }
-  
-  // Make sure we un-register Firebase observers when the component unmounts.
   componentWillUnmount() {
     this.unregisterAuthObserver();
   }
@@ -66,16 +50,16 @@ class SignInScreen extends Component {
       return (
         <div className="container">
           <h1>Medicine Shop</h1>
-          <h1>Please sign-in</h1>
+          <p>Please sign-in</p>
           <StyledFirebaseAuth uiConfig={this.uiConfig} firebaseAuth={firebase.auth()}/>
         </div>
       );
     }
     return (
       <div className="container">
-        <h1>Pharmacy</h1>
-        <h1>Store</h1>
-        <p>Welcome to shop {firebase.auth().currentUser.displayName}! You are now signed-in!</p>
+        <h1>Medicine Shop</h1>
+        <h1>Can Buy Price Member</h1>
+        <p>Welcome {firebase.auth().currentUser.displayName}! You are now signed-in</p>
           <img id="photo" className="pic" src={firebase.auth().currentUser.photoURL}/>
         <button onClick={() => firebase.auth().signOut()}>Sign-out</button>
       </div>
@@ -83,4 +67,11 @@ class SignInScreen extends Component {
   }
 }
 
-export default SignInScreen;
+export default LoginForm;
+
+
+
+
+
+
+
