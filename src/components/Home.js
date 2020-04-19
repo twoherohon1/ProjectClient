29fcb09ca024332e64638b0280340563ba3bb13a
twoherohon1 <twoherohon1@hotmail.com>
2020-04-19 13:24:17 +0700

    CarouselHome

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Button, Form, Nav, FormControl } from 'react-bootstrap';
+import { Navbar, Button, Form, Nav, FormControl,Carousel } from 'react-bootstrap';
 import './Home.css'
 import firebase from 'firebase/app';
 
@@ -7,11 +7,11 @@ import firebase from 'firebase/app';
 
 const Home = (props) => {
   return (
-      <div> 
+    <div>
       <Navbar bg="light" expand="lg">
         <Navbar fixed="top" />
         <Navbar.Brand>
-        <img src="https://i.pinimg.com/originals/79/5f/a2/795fa2a19893756633d929cfc218e0e5.png" width="150"  height="70"></img>
+          <img src="https://i.pinimg.com/originals/79/5f/a2/795fa2a19893756633d929cfc218e0e5.png" width="150" height="70"></img>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -32,18 +32,54 @@ const Home = (props) => {
           <Form inline>
             <Nav.Link
               onClick={() => props.history.push('/login')}
-            >MyProfile 
+            >MyProfile
             </Nav.Link>
-            {firebase.auth().currentUser.email=="twoherohon1@gmail.com"?
-            <Nav.Link
-            onClick={() => props.history.push('/admin')}
-          >AdminEdit 
-          </Nav.Link>:<div></div>}
+            {firebase.auth().currentUser.email == "twoherohon1@gmail.com" ?
+              <Nav.Link
+                onClick={() => props.history.push('/admin')}
+              >AdminEdit
+          </Nav.Link> : <div></div>}
           </Form>
         </Navbar.Collapse>
       </Navbar>
-      <img src="https://cdn4.vectorstock.com/i/1000x1000/56/08/pharmacy-banner-medicine-medical-supplies-vector-15075608.jpg" width="100%" height="400px"></img>
-      </div>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="http://www.brandage.com/images/paragraph/48C8A3C-AF07B3C-80E6B36.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="http://www.brandage.com/images/paragraph/37E97B0-E2E15C3-EE51F33.jpg"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://www.chicrepublicthai.com/media/catalog/product/cache/1/image/800x800/9df78eab33525d08d6e5fb8d27136e95/R/W/RWA055-IRIR-AB1AB1-1_1.jpg"
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
   )
 }
 export default Home
