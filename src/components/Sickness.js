12317e1137ca63e069dc0378a4c00604c1eb9736
twoherohon1 <twoherohon1@hotmail.com>
2020-04-23 18:23:28 +0700

    showsick

--- src/components/Sickness.js
+++ src/components/Sickness.js
@@ -1,10 +1,63 @@
-import React from 'react';
-import { Navbar, Button, Form, Nav, FormControl } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import './Store.css';
+import { firestore } from '../index'
+import { Button, Card, Form, Navbar, Nav, FormControl } from 'react-bootstrap';
 
-const Sickness= (props)=>{
-    return(
-        <>
-        <Navbar bg="light" expand="lg">
+const Store = (props) => {
+
+  const [tasks1, setTasks1] = useState([])
+  const [sickness, setSickness] = useState('')
+  const [symptom, setSymptom] = useState('')
+  const [dispensing, setDispensing] = useState('')
+  const [warning, setWarning] = useState('')
+
+  useEffect(() => {
+    retrivData()
+  }, [])
+
+  const retrivData = () => {
+    firestore.collection("tasks1").onSnapshot((snapshot) => {
+      let myTask = snapshot.docs.map(d => {
+        const { id, dispensing, sickness, symptom,warning } = d.data()
+        return { id, dispensing, sickness, symptom,warning }
+      })
+      setTasks1(myTask)
+
+    })
+  }
+  const rederTask = () => {
+    if (tasks1 && tasks1.length)
+      return tasks1.map((task, index) => {
+        return (
+          <>
+            <Card style={{ width: '50rem', marginTop: 10}}>
+              <Card.Body  style={{display:'flex',flexDirection:'column',justifyContent:'center',alignItems:'center'}} >
+                <Card.Title style={{ju:'center'}}> โรค : {task.sickness}</Card.Title>
+                <Card.Text>
+                  อาการ : {task.symptom}
+                </Card.Text>
+                <Card.Text>
+                  อาการ : {task.dispensing}
+                </Card.Text>
+                <Card.Text>
+                  คำเตือน : {task.warning}
+                </Card.Text>
+                <div  style={{display:'flex',flexDirection : 'row'}}>
+                </div>
+              
+              </Card.Body>
+            </Card>
+
+
+          </>
+        )
+      })
+    else
+      return <li>No Task</li>
+  }
+  return (
+    <>
+       <Navbar bg="light" expand="lg">
         <Navbar fixed="top" />
         <Navbar.Brand>
         <img src="https://i.pinimg.com/originals/79/5f/a2/795fa2a19893756633d929cfc218e0e5.png" width="150"  height="70"></img>
@@ -17,11 +70,11 @@ const Sickness= (props)=>{
               Home
               </Nav.Link>
             <Nav.Link
-              onClick={() => props.history.push('/storeuser')}
-            >Store
+              onClick={() => props.history.push('/editcargo')}
+            >Cargo
             </Nav.Link>
             <Nav.Link
-              onClick={() => props.history.push('/sick')}
+              onClick={() => props.history.push('/editsick')}
             >Sick
             </Nav.Link>
           </Nav>
@@ -33,13 +86,12 @@ const Sickness= (props)=>{
           </Form>
         </Navbar.Collapse>
       </Navbar>
-        <div>
-            <h1>โรค : ไข้หวัด</h1>
-            <h3>อาการ : ปวดหัว ตัวร้อน มีน้ำหมูก มีเสมหะ</h3>
-            <h3>การจ่ายยา :<li>ยาแก้แพ้</li><li>ยาลดไข้</li><li>ยาฆ่าเชื้อ</li></h3>
-            <p>ควรปรึกษาเภสัชก่อนเลือกซื้อ</p>
-        </div>
-        </>
-    )
+      <div style={{ display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'center', alignItems: 'center',backgroundColor:'#99FF99' }}>
+        <h1>แนะนำยาที่ควรซื้อ</h1>
+        <ul style={{ display: 'flex', flex: 1, flexDirection: 'column' }}>{rederTask()}</ul>
+      </div>
+    </>
+  );
 }
-export default Sickness
+
+export default Store;
